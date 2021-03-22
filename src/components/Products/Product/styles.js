f95d94cc58aeb8ddec299b875bd956cc5694aec9
Emanuel <emanueltejada31295@gmail.com>
2021-03-22 14:37:18 -0400

    Add navbar and styles, Basic styles to main products content

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -1,18 +1,19 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({
+  root: {
+    maxWidth: '100%',
+  },
   media: {
-    height: 260,
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  cardActions: {
+    display: 'flex',
+    justifyContent: 'flex-end',
   },
   cardContent: {
     display: 'flex',
     justifyContent: 'space-between',
-  },
-  cartActions: {
-    justifyContent: 'space-between',
-  },
-  buttons: {
-    display: 'flex',
-    alignItems: 'center',
   },
 }));
