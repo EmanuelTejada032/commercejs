d15afefc4192a04006bb62a10efb4ca2ba22825b
Emanuel <emanueltejada31295@gmail.com>
2021-03-24 21:18:27 -0400

    Style address form, create input system

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,10 +1,51 @@
 import React from 'react'
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
+import { useForm, FormProvider } from 'react-hook-form';
+
+import FormInput from './CustomTextField'
 
 const AddressForm = () => {
+    const methods = useForm();
     return (
-        <div>
-            AddressForm
-        </div>
+        <>
+            <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+            <FormProvider {...methods}>
+                <form onSubmit={''}>
+                    <Grid container spacing={3}>
+                        <FormInput required name="firstName" label="First Name" />
+                        <FormInput required name="lastName" label="Last Name" />
+                        <FormInput required name="address" label="Address" />
+                        <FormInput required name="email" label="Email" />
+                        <FormInput required name="city" label="City" />
+                        <FormInput required name="zip" label="ZIP" />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <InputLabel>Shipping Subdivision</InputLabel>
+                        <Select value={''} fullWitdh onChange={''} >
+                            <MenuItem key={''} value={''}>
+                                Select me
+                            </MenuItem>
+                        </Select>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <InputLabel>Shipping Subdivision</InputLabel>
+                        <Select value={''} fullWitdh onChange={''} >
+                            <MenuItem key={''} value={''}>
+                                Select me
+                            </MenuItem>
+                        </Select>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <InputLabel>Shipping Subdivision</InputLabel>
+                        <Select value={''} fullWitdh onChange={''} >
+                            <MenuItem key={''} value={''}>
+                                Select me
+                            </MenuItem>
+                        </Select>
+                    </Grid>
+                </form>
+            </FormProvider>
+        </>
     )
 }
 
